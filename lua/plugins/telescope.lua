17b6a99eaa27e8cfea6454bc272bf50c88bb8b83
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2023-11-30 23:12:20 +0800

    add telescope-undo

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,45 @@
+return {
+  -- Fuzzy Finder (files, lsp, etc)
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      "debugloop/telescope-undo.nvim",
+      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+      -- Only load if `make` is available. Make sure you have the system
+      -- requirements installed.
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        -- NOTE: If you are having trouble with this installation,
+        --       refer to the README for telescope-fzf-native for more instructions.
+        build = 'make',
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+    },
+    keys = {
+      {
+        "<leader>fu",
+        "<cmd>Telescope undo<cr>",
+        desc = "Undo history",
+      },
+    },
+    opts = {
+      defaults = {
+        mappings = {
+          i = {
+            ['<C-u>'] = false,
+            ['<C-d>'] = false,
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require("telescope").setup(opts)
+      require("telescope").load_extension('fzf')
+      require("telescope").load_extension("undo")
+    end
+  },
+}
