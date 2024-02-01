c54b81c4b7aca50598c7a075df7bfbb4d68954e9
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2024-02-01 14:27:40 +0800

    add neogen, move files

--- lua/plugins/lazy_override.lua
+++ lua/plugins/lazy_override.lua
@@ -1,4 +1,8 @@
 return {
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
   {
     "folke/which-key.nvim",
     opts = {
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,76 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = {
+      cmdline = {
+        view = "cmdline",
+        format = {
+          cmdline = { conceal = false },
+          search_down = { conceal = false },
+          search_up = { conceal = false },
+          filter = { conceal = false },
+          lua = { conceal = false },
+          help = { conceal = false },
+        },
+      },
+      lsp = {
+        hover = { silent = true },
+      },
+      routes = {
+        {
+          view = "mini",
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "; after #%d+" },
+              { find = "; before #%d+" },
+              { find = "%d more lines" },
+              { find = "%d lines yanked" },
+              { find = "%d fewer lines" },
+              { find = "%d lines changed" },
+              { find = "%d lines [<>]ed %d time" },
+            },
+          },
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = false,
+        long_message_to_split = true,
+        inc_rename = true,
+        lsp_doc_border = true,
+      },
+    },
+  },
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      {
+        "<leader>e",
+        function()
+          require("oil").toggle_float()
+        end,
+        desc = "Oil file explorer (root dir)",
+      },
+    },
+    opts = {
+      keymaps = {
+        ["q"] = "actions.close",
+      },
+      float = {
+        max_height = 50,
+      },
+    },
+    init = function()
+      if vim.fn.argc(-1) == 1 then
+        local stat = vim.loop.fs_stat(vim.fn.argv(0))
+        if stat and stat.type == "directory" then
+          require("oil")
+          vim.cmd.lcd(require("oil").get_current_dir())
+        end
+      end
+    end,
+  },
+}
