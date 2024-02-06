fe571bbef477bd1dca344e4ecc78823989be2987
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2024-02-06 17:48:44 +0800

    fix tag ending treated as comments in Svelte

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Remove `>` comments for Svelte files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "*.svelte" },
+  callback = function()
+    vim.cmd("setlocal comments-=n:>")
+  end,
+  group = vim.api.nvim_create_augroup("svelte_comment", { clear = true }),
+})
