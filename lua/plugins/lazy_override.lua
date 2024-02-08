f59f715d13234f3d5577328a24d46b0c6c001d2b
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2024-02-08 13:04:09 +0800

    remap nvim-spectre

--- lua/plugins/lazy_override.lua
+++ lua/plugins/lazy_override.lua
@@ -61,4 +61,17 @@ return {
       },
     },
   },
+  {
+    "nvim-pack/nvim-spectre",
+    keys = {
+      { "<leader>sr", false },
+      {
+        "<leader>S",
+        function()
+          require("spectre").open()
+        end,
+        desc = "Replace in files (Spectre)",
+      },
+    },
+  },
 }
