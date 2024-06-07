fbddbefd1339de48bb681decf0cb2271a05b4053
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2024-06-08 00:10:21 +0800

    remove kickstart comments

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,7 +3,6 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   { 'tpope/vim-sleuth', event = 'LazyFile' }, -- Detect tabstop and shiftwidth automatically
 
   { 'HiPhish/rainbow-delimiters.nvim', event = 'LazyFile' },
@@ -12,41 +11,10 @@ return {
 
   { 'Eandrju/cellular-automaton.nvim', cmd = 'CellularAutomaton' },
 
-  -- NOTE: Plugins can also be added by using a table,
-  -- with the first argument being the link and the following
-  -- keys can be used to configure plugin behavior/loading/etc.
-  --
-  -- Use `opts = {}` to force a plugin to be loaded.
-  --
-  --  This is equivalent to:
-  --    require('Comment').setup({})
-
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', event = 'LazyFile' },
 
   { 'windwp/nvim-ts-autotag', event = 'LazyFile' },
-
-  -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
-  --
-  -- This is often very useful to both group configuration, as well as handle
-  -- lazy loading plugins that don't need to be loaded immediately at startup.
-  --
-  -- For example, in the following configuration, we use:
-  --  event = 'VimEnter'
-  --
-  -- which loads which-key before all the UI elements are loaded. Events can be
-  -- normal autocommands events (`:help autocmd-events`).
-  --
-  -- Then, because we use the `config` key, the configuration only runs
-  -- after the plugin has been loaded:
-  --  config = function() ... end
-
-  -- NOTE: Plugins can specify dependencies.
-  --
-  -- The dependencies are proper plugin specifications as well - anything
-  -- you do for a plugin at the top level, you can do for a dependency.
-  --
-  -- Use the `dependencies` key to specify the dependencies of a particular plugin
 
   -- Highlight todo, notes, etc in comments
   {
@@ -150,17 +118,7 @@ return {
     main = 'ibl',
   },
 
-  -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- place them in the correct locations.
-
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
 }
