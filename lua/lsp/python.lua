4f8d4b8db73e33920eec681869f86d1c90694d10
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2024-10-05 00:55:31 +0800

    add venv-selector
    uncommited changes

--- lua/lsp/python.lua
+++ lua/lsp/python.lua
@@ -7,15 +7,22 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       basedpyright = {},
-      ruff_lsp = {
-        keys = {
-          {
-            "<leader>co",
-            require("utils").lsp.action["source.organizeimports"],
-            desc = "Organize Imports",
-          },
-        },
-      },
+      ruff_lsp = {},
+    },
+  },
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim" },
+    branch = "regexp",
+    cmd = "VenvSelect",
+    ft = "python",
+    opts = {
+      parents = 5,
+      name = { "venv", ".venv" },
+    },
+    keys = {
+      -- Keymap to open VenvSelector to pick a venv.
+      { "<leader>lv", "<cmd>VenvSelect<cr>", desc = "Select VirtualEnv", ft = "python" },
     },
   },
 }
